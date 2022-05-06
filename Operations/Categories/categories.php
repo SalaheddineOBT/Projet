e37105cdc8ba611c4d4ed917363f0ffbcafdba93
SalaheddineOBT <91952603+SalaheddineOBT@users.noreply.github.com>
2022-05-06 10:34:14 +0000

    save changes

---
+++ Operations/Categories/categories.php
@@ -0,0 +1,40 @@
+<?php
+    header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: access");
+    header("Access-Control-Allow-Methods: POST");
+    header("Content-Type: application/json; charset=UTF-8");
+    header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+    include_once("./../../DataBase/DataBase.php");
+    $db=new DataBase();
+    $con=$db->Connection();
+    $data=json_decode(file_get_contents("php://input"));
+    if($_SERVER["REQUEST_METHOD"] != "POST"):
+        $db->Message(0,404,"Page Not Found !");
+    elseif(
+        !isset($data->selectedBy) ||
+        empty(trim($data->selectedBy))
+    ):
+        $db->Message(0,422,"Pleas Fill all The Required Fields !");
+    else:
+        try{
+            $selected=$data->selectedBy;
+            if($selected == "ById" && isset($_REQUEST["id"]) && !empty($_REQUEST["id"])):
+                if($s=$db->SelectCategoriesByID($_REQUEST["id"])):
+                    echo json_encode(["success" => 1,'status'=>500, "User" =>$s]);
+                else:
+                    $db->Message(0,401,"No Data With This Id !");
+                endif;
+            elseif($selected == "All"):
+                if($stmt=$db->SelectAllCategories()):
+                    echo json_encode(["success" => 1,'status'=>500, "Users" => $stmt]);
+                else:
+                    $db->Message(0,401,"The DataBase Is Empty !");
+                endif;
+            elseif($selected == "ById" && !isset($_REQUEST["id"]) || empty($_REQUEST["id"])):
+                $db->Message(0,422,"The ID is Required !");
+            endif; 
+        }catch(PDOEception $e){ 
+            $db->Message(0,401,"".$e->getMessage());
+        }
+    endif;
+?>
